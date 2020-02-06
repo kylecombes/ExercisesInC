931ed66950faa304106e1deba05463c2d2f9266a
Kyle Combes <kylecombes@gmail.com>
2020-02-06 14:57:08 -0500

    HW2: Complete Exercise 2 parts 1 and 2.

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -10,17 +10,32 @@ License: GNU GPLv3
 
 int var1;
 
+void run_me() {
+    int var = 4;
+    printf("run_me: var points to %p\n", &var);
+}
+
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
+    void *q = malloc(128);
     char *s = "Hello, World";
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
+    printf ("q points to %p\n", q);
     printf ("s points to %p\n", s);
+
+    run_me();
+
+    void *a = malloc(16);
+    void *b = malloc(16);
+    printf ("a points to %p\n", a);
+    printf ("b points to %p\n", b);
+    // Difference of 20 bytes
 
     return 0;
 }
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -10,29 +10,53 @@ License: GNU GPLv3
 
 #define SIZE 5
 
+// This function creates a new array of integers of length SIZE (5) and sets all
+// of its values to 42. It then returns (a pointer to) the array.
+// My guess is that this won't work as expected because array is stored
+// in the stack and deallocated once the function returns.
 int *foo() {
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+//    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
     }
     return array;
 }
+// Yeah, gcc warns that the variable I'm returning is in a region of the stack
+// due to be deallocated.
 
+// This function creates a new array of integers of length SIZE (5) and sets
+// each value to correspond to its index. It returns nothing.
 void bar() {
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+//    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
     }
 }
 
+// The programmer likely intends for the output to be
+// 42
+// 42
+// 42
+// 42
+// 42
+// but that probably isn't going to be the case, since array points to a location
+// in the stack that has been deallocated. Since foo() and bar() both allocate
+// the same variables, it's possible that the output will be
+// 0
+// 1
+// 2
+// 3
+// 4
+// indicating bar()'s array variable was allocated at the same address in
+// memory as foo()'s was.
 int main()
 {
     int i;
@@ -45,3 +69,8 @@ int main()
 
     return 0;
 }
+// Running this results in a segfault, which seems to indicate that I can't
+// access a part of memory beyond the last stack frame (even if that memory was
+// previously a part of the stack).
+
+// After commenting out the print statements, I still get a segfault. ;(

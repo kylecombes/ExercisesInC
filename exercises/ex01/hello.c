919dc54059976a3f6e37a8f2aa37254c645ec816
Kyle Combes <kylecombes@gmail.com>
2020-02-14 08:30:12 -0500

    HW 2.5: Finish first part.

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
+int test(char c);
+
+int test(char c) {
+    if (c == 'a') {
+        return 5;
+    } else if (c == 'b') {
+        return 2;
+    }
+}
+
 int main() {
-    // This line will converted to assembly both with and without the -O2 flag
-    // when x is referenced in the following line. If x is never accessed, then
-    // only the non-optimized compilation will generate assembly code.
     int x = 5;
-    //printf("Hello, World! %d\n", x);
-    // This line is converted to assembly in the non-optimized compilation, but
-    // not when the -O2 flag is used.
-    int y = x + 1;
-    // When the following line is added, both x and y are kept in the assembly
-    // code in the non-optimized version. In the optimized version, y is
-    // set to be 6 from the get-go and x is dropped entirely.
-    printf("And our number is: %d", y);
+    int *p;
+    *p = x + 1;
     return 0;
 }
--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -7,6 +7,7 @@ Modified version of an example from Chapter 2.5 of Head First C.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -32,12 +33,48 @@ void find_track(char search_for[])
     }
 }
 
-// Finds all tracks that match the given pattern.
-//
-// Prints track number and title.
+/**
+ * Match string against the extended regular expression in
+ * pattern, treating errors as no match.
+ * @param string the string to search
+ * @param pattern the pattern to use when checking the string
+ * @return 1 for match, 0 for no match
+ */
+
+int match(const char *string, char *pattern)
+{
+    int status;
+    regex_t re;
+
+    if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB) != 0) {
+        fprintf(stderr, "An error was encountered when compiling a regex for "
+                         "the pattern '%s'.\n", pattern);
+        exit(1);
+    }
+    // Look for our pattern in the string
+    status = regexec(&re, string, (size_t) 0, NULL, 0);
+    // Free the memory allocated to the pattern buffer
+    regfree(&re);
+    // Check our result code
+    if (status == 0) { // Match!
+        return(1);
+    }
+    // No match :(
+    return(0);
+}
+
+/**
+ * Finds all tracks that match the given pattern and prints the track number
+ * and title of all matches.
+ * @param pattern the regex pattern to match
+*/
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+    for (int i = 0; i < NUM_TRACKS; ++i) {
+        if (match(tracks[i], pattern)) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        }
+    }
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -51,15 +88,16 @@ void rstrip(char s[])
 
 int main (int argc, char *argv[])
 {
+    /*
     char search_for[80];
 
-    /* take input from the user and search */
+    /* take input from the user and search *
     printf("Search for: ");
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    find_track(search_for);*/
+    find_track_regex("[A-F]");
 
     return 0;
 }
